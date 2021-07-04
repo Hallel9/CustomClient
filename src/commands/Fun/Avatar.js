9e2f75093028f20ca3bf1f3d76a31b858a189149
hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-07-04 15:26:23 +0000

    Updated avatar command

--- src/commands/Fun/Avatar.js
+++ src/commands/Fun/Avatar.js
@@ -1,5 +1,6 @@
 const Command = require("../../Build/Command");
 const Embed = require("../../Build/Embed");
+const { GuildMember } = require("discord.js");
 
 module.exports = class extends Command {
   constructor(...args) {
@@ -10,13 +11,30 @@ module.exports = class extends Command {
     });
   }
 
-  async run(message) {
-    const member = message.mentions.members.first() || message.member;
+  async run(message, args) {
+    let user =
+      message.mentions.users.first() ||
+      this.client.users.cache.get(args[0]) ||
+      this.client.users.cache.find(
+        (e) =>
+          e.username.includes(args.join(" ")) || e.tag.includes(args.join(" "))
+      ) ||
+      (message.guild
+        ? this.client.users.cache.find((e) => e.nickname === args.join(" "))
+        : undefined) ||
+      (args[0]
+        ? await this.client.users.fetch(args[0]).catch(() => {})
+        : undefined) ||
+      message.author;
+    if (!user) return; // return something if no user found
+    if (user instanceof GuildMember) {
+      user = user.user;
+    }
 
     message.channel.send(
       new Embed()
-        .setTitle(`${member.user.tag}'s avatar`)
-        .setImage(member.user.displayAvatarURL({ dynamic: true }))
+        .setTitle(`${user.tag}'s avatar`)
+        .setImage(user.displayAvatarURL({ dynamic: true }))
         .setColor("RANDOM")
         .setTimestamp()
     );
